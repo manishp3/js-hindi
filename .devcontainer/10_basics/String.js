dbc11bffd394b97b34c0a31268a4e650ed838a16
manishp3 <128165494+manishp3@users.noreply.github.com>
2023-12-18 16:12:26 +0000

    string function

---
+++ .devcontainer/10_basics/String.js
@@ -0,0 +1,38 @@
+// // console.log("hello"+" world");
+// name="Manish";
+// const repo=50;
+// // old
+// console.log(name +repo +" value");
+// // modern
+// console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+// const gameName=new String('manishbhaipatadiya');
+// console.log((gameName.length));
+// console.log((gameName.toUpperCase));
+// console.log(gameName.charAt('4'));
+// console.log(gameName.indexOf('s'));
+
+// //  don't accept negative value
+// const newS=gameName.substring(2,5)
+// console.log(newS);
+
+// accept negative value
+// const as=gameName.slice(-8,3);
+// console.log(as);
+// let game="          manish            ";
+// console.log(game);
+// const tr=game.trim("");
+// console.log(tr);
+
+// const url="manish@20%gmailgithub";
+
+// console.log(url.replace('20%','na'));
+// console.log(url.includes("hitesh"));
+
+
+const str = 'The quick brown fox jumps over the lazy dog.!';
+
+console.log(str.endsWith('dog!'));
+
+const words = str.split(' ');
+console.log(words[4]);
