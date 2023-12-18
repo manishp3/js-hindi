4f3ff9b82897bb5568cc6e9988579baaedf0b466
manishp3 <128165494+manishp3@users.noreply.github.com>
2023-12-18 17:28:27 +0000

    date functions

---
+++ .devcontainer/01_basics/06_Date.js
@@ -0,0 +1,22 @@
+// let myDate=new Date();
+// console.log((myDate.toDateString()));
+// console.log((myDate.toISOString()));
+// console.log((myDate.toLocaleString()));
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+
+// let mycd=new Date(2023,0,15,12,5);
+// console.log(mycd.toLocaleString());
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+
+// console.log(Math.floor(Date.now()/1000));
+
+let nDate=new Date();
+// console.log(nDate);
+console.log(nDate.getDay());
+
+console.log(nDate.toLocaleString());
