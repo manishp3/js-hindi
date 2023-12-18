b01ffade25c96e3149fd73b82599ea15181489a6
manishp3 <128165494+manishp3@users.noreply.github.com>
2023-12-18 16:50:52 +0000

    math_number

---
+++ .devcontainer/10_basics/05_math_number.js
@@ -0,0 +1,20 @@
+// const scr=500
+// const bal=new Number(100.1234)
+// console.log(scr);
+// console.log(bal);
+// console.log(bal.toFixed(2));
+// // console.log(typeof(bal.toString()));
+// const hun=10000000;
+// console.log(hun.toLocaleString('en-IN'));
+// console.log(hun.toLocaleString());
+let a=45;
+let b=(34,5,6,78,4,2);
+console.log(Math.min(b));
+console.log(Math.max(b));
+
+// ceiling means up
+// console.log(Math.ceil(4.2));
+// flooring means down
+// console.log(Math.floor(4.9));
+
+console.log(Math.random()*23);
