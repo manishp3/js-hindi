993ceb7f6167378c59e9eb1c09cc3bf9e35ac440
manishp3 <128165494+manishp3@users.noreply.github.com>
2023-12-18 18:06:40 +0000

    array

---
+++ .devcontainer/01_basics/07_arrays.js
@@ -0,0 +1,21 @@
+// const myArr=[1,2,4,5,6,78,4];
+// console.log((myArr));
+
+// const myHeros=["shaktiman","marvels","bedmintan"];
+// console.log(myHeros);
+
+// // myArr.push(7)
+
+// myArr.pop();
+// console.log(myArr.includes(3));
+// console.log(myArr);
+
+
+let a=[1,2,3,4,5,676]
+console.log(typeof(a));
+// let b=[45,2,2,1,45,7,56];
+// let c=a.join();
+// console.log(typeof(a));
+
+// console.log(c);
+// console.log(a.includes(3));
