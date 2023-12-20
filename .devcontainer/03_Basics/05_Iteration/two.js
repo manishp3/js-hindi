0a759ea12b8afb59c9ca4ba4a14e800b94fee555
Manish Patadiya <128165494+manishp3@users.noreply.github.com>
2023-12-20 04:19:12 +0000

    foreach

---
+++ .devcontainer/03_Basics/05_Iteration/two.js
@@ -0,0 +1,26 @@
+// const myobj={
+//     js:"javascript",
+//     cpp:"c++",
+//     rb:"rubby",
+//     swft:"swift by apple"
+// }
+// for (const key in myobj) {
+//    console.log(`${key} for ${myobj[key]}`);
+// }
+
+// let frt=['mango','banana','kaela','jamrud','badam']
+// for (const mey in frt) {
+//     console.log(mey +" AND " +frt[mey]);
+// }
+
+
+const mycoding = [
+   { lagName: "Jabascript",
+    filename: ".js"},
+    { lagName1: "Jabascript",
+    filename1: ".js"},
+]
+
+mycoding.forEach((item) => {
+    console.log(item.filename1);
+})
