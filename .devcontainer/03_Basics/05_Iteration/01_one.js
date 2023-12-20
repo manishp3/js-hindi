b67c3190fdda2a6f43c3d81e92a02c094a5c39d2
Manish Patadiya <128165494+manishp3@users.noreply.github.com>
2023-12-20 03:52:02 +0000

    HIgher order

---
+++ .devcontainer/03_Basics/05_Iteration/01_one.js
@@ -0,0 +1,45 @@
+// // for (let i = 0; i <=10; i++) {
+
+// //    if(i==5)
+// //    {
+// //     console.log("Detectd :"+i);
+// //     continue
+// //    }
+// //    console.log(i);
+// // }
+// // let i=1
+// // while (i<=45) {
+// //     console.log(i);
+// //     i++;
+// // }
+
+// let frt=['mango','banana','kaela','jamrud','badam']
+
+// // const m=frt.map((i)=>console.log("The fruit is :",i))
+
+
+const a=[56,2,3,4,6,5,4,78,2,3]
+// for (const num of a) {
+//     console.log("Sqare of it is :",num*num," And Real numner is ",num);
+// // }
+// const m=a.map((num)=> console.log("Sqare of it is :",num*num," And Real numner is ",num))
+// console.log(m);
+// const map=new Map()
+// map.set('In','India')
+// map.set('us','amerika')
+// map.set('en','England')
+// map.set('In','India')
+// for (const [key,val] of map) {
+//     console.log(key +" "+val);
+    
+// }
+// console.log(map.keys);
+
+const games={
+    "game1":"spider",
+    "game2":"car racing",
+    "game3":"bike racing"
+}
+for (const [key,val] of games) {
+    console.log(key,":=>",val);
+}
