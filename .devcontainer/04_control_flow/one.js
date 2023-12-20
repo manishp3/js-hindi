c6d7dc885547d011241edd22e6b6353983e9998d
Manish Patadiya <128165494+manishp3@users.noreply.github.com>
2023-12-20 05:24:33 +0000

    flow of execution

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,16 @@
+let bal=1000
+
+// if(bal>500) console.log("test"),console.log("test2");
+ if(bal>600){
+    console.log("> than  600");
+}
+else if(bal>550){
+console.log("greater THan 500");
+}
+else if(bal<750){
+    console.log("less than 750");
+}
+
+else{
+    console.log("Greater than 750");
+}
