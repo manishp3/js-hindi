f99468870abcae6c89735be2cce20ef381aa2d40
Manish Patadiya <128165494+manishp3@users.noreply.github.com>
2023-12-22 04:10:19 +0000

    Objects

---
+++ 05_Iteration/Object.js
@@ -0,0 +1,15 @@
+// function mulit(num){
+    // this.num=num   //done by js under the hood
+//     return num*5
+// }
+
+// mulit.power=4
+
+// console.log(mulit(5));
+// console.log(mulit.power);
+// console.log(mulit.prototype);
+
+function createUser(uname,score){
+    this.uname=uname;
+    this.score=score
+}
